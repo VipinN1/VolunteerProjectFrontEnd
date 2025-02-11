df098e0ddff7f2bcb721b031d6c8da0604959107
HRGhozali <111896897+HRGhozali@users.noreply.github.com>
2025-02-10 23:41:47 -0600

    Commit 1.0.1
    Further edits to the login page and CSS. Added a Log In and Register button to the top header, and changed the header and footer box backgrounds.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,24 +8,30 @@ function App() {
 
   return (
     <>
-      <div id="header_div">
+      <div id="header_div"> {/* Add a header_div to all pages w/ "Volunteer Site" in it; don't have the buttons for any page except the account register one as the rest should require a user to be logged in*/}
         Volunteer Site
+        <button id="login_button">Log In</button>
+        <button id="register_button">Register</button>
       </div>
-      <h1>User Login</h1>
-      <p>
-        <label for="username">Username </label>
-        <input type="text" id="username" name="username" size="10" required></input>
-      </p>
-      <p>
-        <label for="password">Password </label>
-        <input type="password" id="password" name="password" size="10" required></input>
-      </p>
-      <p>
-        <input type="submit" value="Login"></input> {/* Add a link to one of the other pages here*/}
-      </p>
-      <section>
-        <p>Not a member yet? <a>Register</a></p> {/* Add a link to the account creation page here */}
-      </section>
+      
+      <div>
+        <h1>Log Into Your Account</h1>
+        <p>
+          <label for="username">Username </label>
+          <input type="text" id="username" name="username" size="15" required></input>
+        </p>
+        <p>
+          <label for="password">Password </label>
+          <input type="password" id="password" name="password" size="15" required></input>
+        </p>
+        <p>
+          <input type="submit" value="Login"></input> {/* Add a link to one of the other pages here*/}
+        </p>
+        <section>
+          <p>Not a member yet? <a>Register</a></p> {/* Add a link to the account creation page here */}
+        </section>
+      </div>
+      <div id="footer_div"></div>  {/* Add a footer_div to all pages w/ "Volunteer Site" in it */}
     </>
   )
 }
