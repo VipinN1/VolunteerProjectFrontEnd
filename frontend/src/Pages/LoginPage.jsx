9b9a0df796694b098ac2c243db52af4a939a7f25
HRGhozali <111896897+HRGhozali@users.noreply.github.com>
2025-02-13 23:05:41 -0600

    Commit 1.0.7
    Updated visuals of the login and register pages. Added a forgot password link (currently placeholder) to login page.

--- frontend/src/Pages/LoginPage.jsx
+++ frontend/src/Pages/LoginPage.jsx
@@ -9,21 +9,22 @@ function LoginPage() {
       <a href="/register/"><button id="register_button_log">Register</button></a>
       
       <div id="body_div">
-        <h1>Log Into Your Account</h1>
+        <h1 id="login_h1">Log Into Your Account</h1>
         <form id="login_form" action="/profile/">
-            <p>
-                <label for="username">Username:</label>
-                <input type="text" id="username" name="username" size="15" required></input>
+            <p id="login_p">
+                <label for="username_log">Username:</label>
+                <input type="text" id="username_log" name="username_log" size="15" required></input>
             </p>
-            <p>
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" size="15" required></input>
+            <p id="login_p">
+                <label for="password_log">Password:</label>
+                <input type="password" id="password_log" name="password_log" size="15" required></input>
             </p>
-            <p>
+            <p id="login_p">
                 <input type="submit" value="Login"></input>  {/* TODO: Figure out the major issue with freakin'... CSS SPILLOVER */}
             </p>
         </form>
         <section>
+        <p id="forgot_msg"><a>Forgot your password?</a></p> {/* Add a link to the account creation page here */}
           <p id="register_msg">Not a member yet? <a href="/register/">Register</a></p> {/* Add a link to the account creation page here */}
         </section>
       </div>
--- frontend/src/Pages/RegisterPage.jsx
+++ frontend/src/Pages/RegisterPage.jsx
@@ -7,7 +7,7 @@ function RegisterPage() {
       <a href="/login/"><button id="login_button_reg">Log In</button></a>
         <a href="/register/"><button id="register_button_reg">Register</button></a>
       
-      <div id="body_div">
+      <div id="body_div_reg">
         <h1>Create your account!</h1>
         <form id="register_form" action="/profile/">
             <p>
@@ -26,6 +26,8 @@ function RegisterPage() {
               <input type="submit" value="Register"></input>
             </p>
         </form>
+        <br></br>
+        <br></br>
         <br></br>
       </div>
       
