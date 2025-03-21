cc0dc65d1c095306654ac462b6efe697f52b6cde
HRGhozali <111896897+HRGhozali@users.noreply.github.com>
2025-03-21 00:34:19 -0500

    Commit 1.0.0
    Initial (IN PROGRESS) attempt to do login stuff with the database. CONNECTION DOES NOT WORK.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const cors = require("cors");
+const sql = require("mssql");
+const bcrypt = require("bcrypt")
 
 const app = express();
 const PORT = 5000;
+const pwdSaltRounds = 10;
 
 app.use(cors());
 app.use(express.json());
+
+// SQL Server configuration - FIGURE THIS OUT
+var config = {
+    "user": "vpserver", // Database username
+    "password": "server", // Database password
+    "server": "localhost\\SQLEXPRESS", // Server IP address
+    "database": "VolunteerDB", // Database name
+    "options": {
+        "encrypt": false // Disable encryption
+    }
+}
+
+// Connect to SQL Server
+sql.connect(config, err => {
+    console.log("Connecting to database")
+    if (err) {
+        throw err;
+    }
+    console.log("Connection Successful!");
+});
+
 
 const eventRoutes = require("./routes/eventRoutes");
 const volunteerRoutes = require("./routes/volunteerRoutes");
@@ -39,15 +63,33 @@ app.post("/api/register", (req, res) => {
     if (!username || !email || !password) {
         return res.status(400).json({ message: "All fields are required" });
     }
+    /*
     if (storedLogins.usernames.includes(username)) {
         return res.status(400).json({ message: "This username is already in use!" });
     }
     if (storedLogins.emails.includes(email)) {
         return res.status(400).json({ message: "This email address is already in use!" });
     }
+        */
+
+    // Generates password salt for encryption
+    bcrypt.genSalt(pwdSaltRounds, function(err, salt) {bcrypt.hash(password, salt, function(err,hash) {});});
+
+    // Inserts new user into Users; if an error occurs, returns the database error
+    sql.connect(config, function(err) {
+        let request = new sql.Request();
+        request.query(`INSERT INTO Users VALUES(${username}, ${hash}, ${email}`, function(err) {  // If this needs to input FullName, add ', ${username})' near the end of the insert part
+            if (err) {
+                return res.status(401).json({message: `Database invalid error: ${err}`});
+            }
+        })
+    })
+
+    /*
     storedLogins.usernames.push(username);
     storedLogins.passwords.push(password);
     storedLogins.emails.push(email);
+    */
     res.status(201).json({ message: "Registered new user successfully", profile: storedLogins });
 });
 
@@ -56,11 +98,31 @@ app.post("/api/login", (req, res) => {
     if (!username || !password) {
         return res.status(400).json({ message: "Both username and password are required" });
     }
+    let userData;
+    let request = new sql.Request();
+    request.query('SELECT [Username, PasswordHash] FROM Users', function(err, data) {
+        if (err) {
+            return res.status(401).json({message: `Database invalid error: ${err}`});
+        }
+        userData = data;
+    });
+    for (const loginData in userData) {
+        bcrypt.compare(password, loginData["PasswordHash"], function(err, result) {
+            if (result && username == loginData["Username"]) {
+                res.status(200).json({ message: "Login successful", username });
+            }
+            else {
+                res.status(401).json({message: "Invalid username/password combination!"})
+            }
+        })
+    }
+
+    /*
     const userIndex = storedLogins.usernames.indexOf(username);
     if (userIndex === -1 || storedLogins.passwords[userIndex] !== password) {
         return res.status(401).json({ message: "Invalid username/password combination!" });
-    }
-    res.status(200).json({ message: "Login successful", username });
+    }*/
+    
 });
 
 app.get("/api/login", (req, res) => {
